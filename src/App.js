146d27e5608c106503e53af82a3c300d152d566e
Adi Narayan <adinarayanrs777@gmail.com>
2025-03-30 23:25:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React from 'react';
+import Navigation from './customer/components/Navigation/Navigation'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+    </div>
+  );
+}
+
+export default App;
